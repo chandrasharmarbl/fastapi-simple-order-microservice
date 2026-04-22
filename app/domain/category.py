9c37d11f0cc9ce12f5741318f6cb88caee9ff33b
Chandra Kant Sharma <chandrasharma@Chandras-MacBook-Pro.local>
2026-04-22 15:59:19 +0530

    feat: added category and item model

---
+++ app/domain/category.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel
+from typing import Optional
+
+
+class CategoryCreate(BaseModel):
+    name: str
+    description: Optional[str] = None
+
+
+class Category(CategoryCreate):
+    id: int
--- app/domain/item.py
+++ app/domain/item.py
@@ -1,19 +1,21 @@
 from typing import Optional
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class ItemCreate(BaseModel):
-    name: str
+    name: str = Field(..., min_length=1)
     description: Optional[str] = None
-    price: float
-    quantity: int = 0
+    price: float = Field(..., gt=0)
+    quantity: int = Field(0, ge=0)
+    category_id: Optional[int] = None
 
 
 class ItemUpdate(BaseModel):
-    name: Optional[str] = None
+    name: Optional[str] = Field(None, min_length=1)
     description: Optional[str] = None
-    price: Optional[float] = None
-    quantity: Optional[int] = None
+    price: Optional[float] = Field(None, gt=0)
+    quantity: Optional[int] = Field(None, ge=0)
+    category_id: Optional[int] = None
 
 
 class Item(ItemCreate):
