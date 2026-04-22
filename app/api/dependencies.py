3f75e0c85e6c93e493d026ade4b68d42c0832b72
Chandra Kant Sharma <chandrasharma@Chandras-MacBook-Pro.local>
2026-04-22 16:35:23 +0530

    refactor: Dependencies adhere to DIP

--- app/api/dependencies.py
+++ app/api/dependencies.py
@@ -1,27 +1,35 @@
 from fastapi import Depends
+from app.domain.interfaces import ItemRepositoryProtocol, AnalyticsClientProtocol
 from app.infrastructure.repository import InMemoryItemRepository
+from app.infrastructure.sql_repository import SQLItemRepository
 from app.infrastructure.analytics_client import AsyncAnalyticsClient
 from app.services.item_service import ItemService
 from sqlalchemy.ext.asyncio import AsyncSession
 from collections.abc import AsyncGenerator
 from app.core.database import async_session_maker
 
-# Singletons to preserve state and connections across requests
-_repository = InMemoryItemRepository()
+# Singletons for memory repositories and clients
+_memory_repository = InMemoryItemRepository()
 _analytics_client = AsyncAnalyticsClient(base_url="http://localhost:8001")
 
 
-def get_item_repository() -> InMemoryItemRepository:
-    return _repository
+async def get_db_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        yield session
 
 
-def get_analytics_client() -> AsyncAnalyticsClient:
+def get_sql_item_repository(session: AsyncSession = Depends(get_db_session)) -> ItemRepositoryProtocol:
+    return SQLItemRepository(session)
+
+
+def get_analytics_client() -> AnalyticsClientProtocol:
     return _analytics_client
 
 
 def get_item_service(
-    repository: InMemoryItemRepository = Depends(get_item_repository),
-    analytics_client: AsyncAnalyticsClient = Depends(get_analytics_client)
+    # DEPENDENCY INVERSION: Depending on Protocol instead of Concrete Class
+    repository: ItemRepositoryProtocol = Depends(get_sql_item_repository),
+    analytics_client: AnalyticsClientProtocol = Depends(get_analytics_client)
 ) -> ItemService:
     return ItemService(repository=repository, analytics_client=analytics_client)
 
